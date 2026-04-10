0296ee76ffedd487ecaeae479ce7037af3783a5a
tanmayyyy <tanmaykhanna07@gmail.com>
2026-04-10 10:33:54 +0530

    Starting with a license manager project

---
+++ src/main/java/com/company/license_manager/LicenseManagerApplication.java
@@ -0,0 +1,13 @@
+package com.company.license_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenseManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenseManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/license_manager/LicenseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.license_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicenseManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
